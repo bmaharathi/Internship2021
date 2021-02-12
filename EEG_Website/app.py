a6213c408a648cdbe224bc1eda6dff3a5c740ceb
adityadasgupta <aditya.dasgupta1999@gmail.com>
2021-02-12 16:06:26 -0600

    flask accepts .rec and .edf files

--- EEG_Website/app.py
+++ EEG_Website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        eeg_file = request.files['eeg_file']
+        if eeg_file.filename != '':
+            eeg_file.save(eeg_file.filename)
+        return redirect(url_for('hello_world'))
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
