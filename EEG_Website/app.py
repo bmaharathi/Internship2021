453f225d97240ef350a2630f8e4e6fc75f988bd8
Tarush Gupta <44489499+tgupta22@users.noreply.github.com>
2021-02-13 13:22:03 -0600

    Accepts .*ref, .*edf

--- EEG_Website/app.py
+++ EEG_Website/app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return render_template("index.html")
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def upload_file():
+    return render_template('index.html')
 
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def openEEGFILE():
+    if request.method == 'POST':
+        f = request.files['eeg_file_form']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 if __name__ == '__main__':
     app.run()
