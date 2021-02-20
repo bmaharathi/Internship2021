dfd711a34c40ebb1d8e77ab21d4dfa2518e2e27f
hajiler <elijahrrbeltran@gmail.com>
2021-02-20 19:52:56 -0400

    Remformats file according to what pycharm said

--- EEG_Website/app.py
+++ EEG_Website/app.py
@@ -1,13 +1,13 @@
-
 from flask import Flask, render_template, redirect, url_for, request, jsonify, after_this_request
 from edfreader import EDFreader
 
-filename = '' #reusable filename variable
+filename = ''  # reusable filename variable
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
-   return render_template("index.html")
-
+    return render_template("index.html")
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -15,11 +15,12 @@ def upload_file():
     if request.method == 'POST':
         eeg_file = request.files['eeg_file']
         global filename
-        filename = '%s'%eeg_file.filename
+        filename = '%s' % eeg_file.filename
         if filename != '':
             eeg_file.save(eeg_file.filename)
         return redirect(url_for('hello_world'))
     return render_template("index.html")
+
 
 @app.route('/electrode_send', methods=['GET'])
 def electrode_send():
@@ -31,7 +32,7 @@ def electrode_send():
     hdl = EDFreader(filename)
     n_elec = hdl.getNumSignals()
     e_dict = {}
-    for i in range(0,n_elec):
+    for i in range(0, n_elec):
         e_dict[i] = hdl.getSignalLabel(i)
     return jsonify(e_dict)
 
