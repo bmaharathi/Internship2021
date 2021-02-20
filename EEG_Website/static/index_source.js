792b4027ad43ed4bc13fda0d721c67f4faa4a604
hajiler <elijahrrbeltran@gmail.com>
2021-02-20 16:53:01 -0400

    Cleans up indenting on index.html to be tab instead of double space. Remove log from open file function

--- EEG_Website/static/index_source.js
+++ EEG_Website/static/index_source.js
@@ -1,6 +1,4 @@
 function openEEGFILE() {
     var fileForm = document.getElementById('eeg_file_form');
     fileForm.style.display = (fileForm.style.display === 'none') ? 'block' : 'none';
-    console.log('File opened');
 }
-
