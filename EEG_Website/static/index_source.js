26644382e2a61b225cf46f890ff2301f9d29aadc
hajiler <elijahrrbeltran@gmail.com>
2021-02-20 19:16:05 -0400

    Adds a form element for selecting electrodes. Some javascript to call on page load. Reveals form based on url parameter"

--- EEG_Website/static/index_source.js
+++ EEG_Website/static/index_source.js
@@ -1,4 +1,17 @@
-function openEEGFILE() {
-    var fileForm = document.getElementById('eeg_file_form');
+function loadIndexPage() {
+    const queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    if (urlParams.has("electrodes")) {
+        openElectrodeSelect();
+    }
+}
+function openFileSelect() {
+    const fileForm = document.getElementById('file_form');
     fileForm.style.display = (fileForm.style.display === 'none') ? 'block' : 'none';
 }
+
+function openElectrodeSelect() {
+    const electrodeForm = document.getElementById('electrode_form');
+    electrodeForm.style.display = (electrodeForm.style.display === 'none') ? 'block' : 'none';
+}
+
