80281f037f38ba09810bbc730446fb28d31871a6
hajiler <elijahrrbeltran@gmail.com>
2021-02-21 12:46:53 -0400

    Adds in sessions to keep track of information. Will be more useful if we ever deploy. Adds post request for selecting forms

--- EEG_Website/app.py
+++ EEG_Website/app.py
@@ -1,29 +1,32 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify, after_this_request
-from edfreader import EDFreader
+from flask import Flask, render_template, redirect, url_for, request, jsonify, after_this_request, session
+import edf_manager
 
 filename = ''  # reusable filename variable
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "CHANGE BEFORE DEPLOYMENT!!!!!"
 
 
+# HOME PAGE: NO FILE TO DISPLAY
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/upload_eeg', methods=['POST', 'GET'])
+# POST EEG FILE
+@app.route('/upload_eeg', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         eeg_file = request.files['eeg_file']
-        global filename
-        filename = '%s' % eeg_file.filename
-        if filename != '':
+        if eeg_file.filename != '':
             eeg_file.save(eeg_file.filename)
         else:
             return redirect(url_for('index'))
-        return redirect(url_for('index', electrodes=True ))
-    return render_template("index.html")
+
+        session['filename'] = eeg_file.filename
+        return redirect(url_for('index', electrodes=True))
 
 
+# GET ELECTRODES TO CHOOSE FROM
 @app.route('/electrode_get', methods=['GET'])
 def electrode_send():
     @after_this_request
@@ -31,13 +34,19 @@ def electrode_send():
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
-    hdl = EDFreader(filename)
-    n_elec = hdl.getNumSignals()
-    e_dict = {}
-    for i in range(0, n_elec):
-        e_dict[i] = hdl.getSignalLabel(i)
-    return jsonify(amount=len(e_dict.keys()),
-                   values=e_dict)
+    return edf_manager.get_electrodes((session['filename']))
+
+
+# POST SELECTED ELECTRODES
+@app.route('/electrode_select', methods=['POST'])
+def selecting_electrodes():
+    selected = []
+    for id in request.form.keys():
+        selected.append(id)
+    for id in request.args.items():
+        selected.append(id)
+    print( list(request.form.keys()),app.logger)
+    return jsonify(selected)
 
 
 if __name__ == '__main__':
