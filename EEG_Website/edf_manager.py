34f5bc0698f060611d4153ceca6b291d138b34aa
Tarush Gupta <44489499+tgupta22@users.noreply.github.com>
2021-02-22 18:44:30 -0600

    Generates Datapoints

---
+++ EEG_Website/edf_manager.py
@@ -0,0 +1,32 @@
+from edfreader import EDFreader
+from flask import jsonify
+import numpy as np
+
+
+# PARSE AVAILABLE ELECTRODES AND RETURN DICTIONARY => INDEX : ELECTRODE NAME
+def get_electrodes(session):
+    hdl = EDFreader(session['filename'])
+    n_elec = hdl.getNumSignals()
+    e_dict = {}
+    for i in range(0, n_elec):
+        e_dict[i] = hdl.getSignalLabel(i)
+    return jsonify(amount=len(e_dict.keys()),
+                   values=e_dict)
+
+
+def get_electrode_date(session):
+    hdl = EDFreader(session['filename'])
+    N = int(session['duration'])
+    data = {}
+    # for each signal in edf file
+    for id in session['selected_id']:
+        signal = int(id)
+        # buffer to hold samples for single signal
+        buf = np.zeros(N)
+        # read N samples for signal
+        hdl.readSamples(signal, buf, N)
+        # invert data
+        buf = buf * (-1)
+        # Add data to list
+        data[hdl.getSignalLabel(signal)] = list(buf)
+    return jsonify(data=data)
