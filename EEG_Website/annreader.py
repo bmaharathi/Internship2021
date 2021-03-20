2c73c9f003b29c4335529f071d7beaf5fc94d6f1
hajiler <elijahrrbeltran@gmail.com>
2021-03-20 14:51:40 -0400

    Adapts annreader to be compatitble with time_series x axis, utilizing onset and duration to find exact time placements.

--- EEG_Website/annreader.py
+++ EEG_Website/annreader.py
@@ -1,22 +1,38 @@
 from flask import jsonify
 import csv
+from edf_manager import get_file_start
+from datetime import datetime, timedelta
 
-# parse csv file into json object
+
+def get_annotations(session):
+    start_time = get_file_start(session)
+    parsed = parse_annotation_file(session)
+    return map_annotations_to_time(parsed, start_time, session['amplitude'])
+
+
+# parse csv file into list of annoations
 def parse_annotation_file(session):
     filename = session['annotation_file']
-    lst = []
     with open(filename, 'r') as csvfile:
         hdl = csv.reader(csvfile)
-        for rows in hdl:
-            lst.append(rows)
+        next(csvfile)
+        lst = [{'Onset': rows[0], 'Duration': rows[1], 'Annotation': rows[2]} for rows in hdl]
 
-    ann_dict = {'Onset': [i[0] for i in lst[1:]], 'Duration': [i[1] for i in lst[1:]],
-                'Annotation': [i[2] for i in lst[1:]]}
-    session = session.update(ann_dict)
-
-    return jsonify(Onset = [i[0] for i in lst[1:]],
-                   Duration = [i[1] for i in lst[1:]],
-                   Annotation = [i[2] for i in lst[1:]])
+    session['annotations'] = lst
+    return lst
 
 
+def map_annotations_to_time(annotations, start_time, amplitude):
+    mapping = []
+    for annotation in annotations:
+        info = {}
+        start = start_time + timedelta(milliseconds=int(annotation['Onset']))
+        end = start + timedelta(milliseconds=int(annotation['Duration']))
 
+        info['start'] = str(start.time())
+        info['end'] = str(end.time())
+        info['label'] = annotation['Annotation']
+        mapping.append(info)
+
+    return jsonify(annotations=mapping,
+                   amplitude=amplitude)
